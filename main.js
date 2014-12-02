b2eaff65aed2092ef29a8d61370cab7e4fb9231e
zhuzihope <liuwenzhu2012@gmail.com>
2014-12-02 17:25:21 -0500

    like button, title, link, pictures

--- main.js
+++ main.js
@@ -14,3 +14,36 @@ $(document).ready(function() {
 		
 	});
 });
+
+$(document).ready(function() {
+	var counter = 0;
+
+    $('.circle').on("click", function(e) {
+    	
+    	if (counter % 2 === 0){		
+			$(this).css({"border": "3px solid #b3f900"});
+		
+		} else {
+			$(this).css({"border": "1px solid #bbb"});
+		}
+
+		counter = counter+1;
+        e.preventDefault();
+    });
+});
+
+
+
+$(document).ready(function() {
+	$('a>.like').on({
+	    'click': function(e) {
+	        var src = ($(this).attr('src') === 'img/like.png')
+	            ? 'img/liked.png'
+	            : 'img/like.png';
+	        $(this).attr('src', src);
+	        e.preventDefault();
+	    }
+	});
+});
+
+
